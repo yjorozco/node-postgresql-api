c4650e9506f46c215e307e1deec4b9d5d1ec3bbd
Yamil Orozco <yamiloro@gmail.com>
2019-09-22 22:23:01 -0430

    adding new feachures;

---
+++ src/app.js
@@ -0,0 +1,14 @@
+import express, {json} from 'express';
+import morgan from 'morgan';
+
+import projectRoutes from './routes/project';
+import taskRoutes from './routes/tasks';
+const app = express();
+
+app.use(morgan('dev'));
+app.use(json());
+
+app.use('/api/projects', projectRoutes);
+app.use('/api/tasks', taskRoutes);
+
+export default app;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-const hello="hello world";
+import app from './app';
+
+async function main(){
+    await app.listen(3000);
+    console.log('server on port 3000')
+}
+
+main();
